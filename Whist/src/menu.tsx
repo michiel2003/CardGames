51cc7709ffcf0f9b9bae9e90ff253921aeb22ae7
MichielDeCapThomasMore <r0879855@student.thomasmore.be>
2022-08-29 14:35:57 +0200

    added card assets
    added bootstrap the correct way
    added font-awesome the correct way
    
    added options gear on top right of whist game main menu screen

--- Whist/src/menu.tsx
+++ Whist/src/menu.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(faGear)
+
 import "./menu.css";
 import './assets/bg.jpg'
 
 // new react component
 const Menu = () => {
     return (
-        <div>
-            <h1>Menu</h1>
+        <div className="d-flex justify-content-end">
+            <h1 className="options">
+                {/* use font awesome gear */}
+                <FontAwesomeIcon size="xs" icon={['fas', 'gear']} />
+            </h1>
             {/* A buttonw with class centered*/}
             <div className="button-container">
                 <button className="yellow-button btn">Start</button>
